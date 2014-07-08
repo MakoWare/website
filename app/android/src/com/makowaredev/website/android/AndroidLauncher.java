d450772f8418d36ad02f4a55369b3a3dcd7347bf
yintothayang <yintothayang@gmail.com>
2014-07-08 09:38:19 -0700

    init new LibGDX project for the website

---
+++ app/android/src/com/makowaredev/website/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.makowaredev.website.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.makowaredev.website.Site;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Site(), config);
+	}
+}
