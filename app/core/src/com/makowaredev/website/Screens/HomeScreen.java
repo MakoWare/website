aacf3820e5059ace2c635053b89d60ccc2e077f3
yintothayang <yintothayang@gmail.com>
2014-07-11 09:49:30 -0700

    formatting and messing around

--- app/core/src/com/makowaredev/website/Screens/HomeScreen.java
+++ app/core/src/com/makowaredev/website/Screens/HomeScreen.java
@@ -11,49 +11,49 @@ import com.badlogic.gdx.math.Vector3;
 public class HomeScreen extends  ScreenAdapter {
 
     Site game;
-	OrthographicCamera guiCam;
-	Rectangle soundBounds;
-	Rectangle playBounds;
-	Rectangle helpBounds;
-	Vector3 touchPoint;
+    OrthographicCamera guiCam;
+    Rectangle soundBounds;
+    Rectangle playBounds;
+    Rectangle helpBounds;
+    Vector3 touchPoint;
 
-	public HomeScreen (Site game){
-		this.game = game;
+    public HomeScreen (Site game){
+        this.game = game;
 
-		guiCam = new OrthographicCamera(320, 480);
-		guiCam.position.set(320 / 2, 480 / 2, 0);
-		soundBounds = new Rectangle(0, 0, 64, 64);
-		playBounds = new Rectangle(160 - 150, 200 + 18, 300, 36);
-		helpBounds = new Rectangle(160 - 150, 200 - 18 - 36, 300, 36);
-		touchPoint = new Vector3();
-	}
+        guiCam = new OrthographicCamera(320, 480);
+        guiCam.position.set(320 / 2, 480 / 2, 0);
+        soundBounds = new Rectangle(0, 0, 64, 64);
+        playBounds = new Rectangle(160 - 150, 200 + 18, 300, 36);
+        helpBounds = new Rectangle(160 - 150, 200 - 18 - 36, 300, 36);
+        touchPoint = new Vector3();
+    }
 
-	public void update () {
+    public void update () {
 
-	}
+    }
 
-	public void draw () {
-		GL20 gl = Gdx.gl;
-		gl.glClearColor(1, 0, 0, 1);
-		gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		guiCam.update();
-		game.batcher.setProjectionMatrix(guiCam.combined);
+    public void draw () {
+        GL20 gl = Gdx.gl;
+        gl.glClearColor(1, 0, 0, 1);
+        gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        guiCam.update();
+        game.batcher.setProjectionMatrix(guiCam.combined);
 
 
 
-		game.batcher.enableBlending();
-		game.batcher.begin();
-		game.batcher.end();
-	}
+        game.batcher.enableBlending();
+        game.batcher.begin();
+        game.batcher.end();
+    }
 
-	@Override
-	public void render (float delta) {
-		update();
-		draw();
-	}
+    @Override
+    public void render (float delta) {
+        update();
+        draw();
+    }
 
-	@Override
-	public void pause () {
+    @Override
+    public void pause () {
 
-	}
+    }
 }
