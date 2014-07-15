fb86edbb49e49f9d05b00aa692d8032609d67970
yintothayang <yintothayang@gmail.com>
2014-07-14 21:33:25 -0700

    programming without 2 screens isn't very fun when you need to constantly be looking at Docs

--- app/core/src/com/makowaredev/website/Screens/HomeScreen.java
+++ app/core/src/com/makowaredev/website/Screens/HomeScreen.java
@@ -14,56 +14,107 @@ public class HomeScreen extends  ScreenAdapter {
 
     Site game;
 
-	OrthographicCamera guiCam;
-	Vector3 touchPoint;
-	World world;
-	WorldListener worldListener;
-    WorldRenderer renderer;
-    int lastScore;
-	String scoreString;
+    OrthographicCamera camera;
+    World world;
 
-    public HomeScreen (Site game){
+    public HomeScreen(final Site game) {
         this.game = game;
 
-        guiCam = new OrthographicCamera(320, 480);
-        guiCam.position.set(320 / 2, 480 / 2, 0);
-        touchPoint = new Vector3();
+        // create the camera and the SpriteBatch
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
 
-        worldListener = new WorldListener() {
+        // create a Rectangle to logically represent the bucket
+        bucket = new Rectangle();
+        bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
+        bucket.y = 20; // bottom left corner of the bucket is 20 pixels above
+        // the bottom screen edge
+        bucket.width = 64;
+        bucket.height = 64;
 
-        };
-
-        world = new World(worldListener);
-        renderer = new WorldRenderer(game.batcher, world);
-
+        // create the raindrops array and spawn the first raindrop
+        raindrops = new Array<Rectangle>();
 
     }
 
-    public void update (float deltaTime) {
-
-    }
-
-    public void draw () {
-        GL20 gl = Gdx.gl;
-        gl.glClearColor(1, 0, 0, 1);
-        gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        guiCam.update();
-        game.batcher.setProjectionMatrix(guiCam.combined);
-
-
-        game.batcher.enableBlending();
-        game.batcher.begin();
-        game.batcher.end();
+    private void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800 - 64);
+        raindrop.y = 480;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
     }
 
     @Override
-    public void render (float delta) {
-        update(delta);
-        draw();
+    public void render(float delta) {
+        // clear the screen with a dark blue color. The
+        // arguments to glClearColor are the red, green
+        // blue and alpha component in the range [0,1]
+        // of the color to be used to clear the screen.
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // tell the camera to update its matrices.
+        camera.update();
+
+        // tell the SpriteBatch to render in the
+        // coordinate system specified by the camera.
+        game.batch.setProjectionMatrix(camera.combined);
+
+
+        game.batch.begin();
+
+        game.batch.end();
+
+
+        // check if we need to create a new raindrop
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000)
+            spawnRaindrop();
+
+        // move the raindrops, remove any that are beneath the bottom edge of
+        // the screen or that hit the bucket. In the later case we increase the
+        // value our drops counter and add a sound effect.
+        Iterator<Rectangle> iter = raindrops.iterator();
+        while (iter.hasNext()) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (raindrop.y + 64 < 0)
+                iter.remove();
+            if (raindrop.overlaps(bucket)) {
+                dropsGathered++;
+                dropSound.play();
+                iter.remove();
+            }
+        }
     }
 
     @Override
-    public void pause () {
+    public void resize(int width, int height) {
+    }
+
+
+    @Override
+    public void show() {
+        // When the screen is shown
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
 
     }
+
 }
