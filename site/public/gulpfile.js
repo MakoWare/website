cc020ff0e620e5bc44d5535ad819dc9b09dcb4aa
yintothayang <yintothayang@gmail.com>
2014-11-26 17:33:03 -0800

    think I figured out the browserify bug

--- site/public/gulpfile.js
+++ site/public/gulpfile.js
@@ -1,69 +1,71 @@
 var gulp = require('gulp'),
-    es6ify = require('es6ify'),
-    $ = require('gulp-load-plugins')();
+  es6ify = require('es6ify'),
+  $ = require('gulp-load-plugins')(),
+  resolve = require('browser-resolve');
 
 gulp.task('js', function () {
-    gulp.src([
-        'components/mako-cube/src/mako-cube.js',
-        'components/mako-cube/src/mako-cube.raw.js'
-    ])
-        .pipe($.plumber())
-        .pipe($.browserify({
-            add: [ es6ify.runtime ],
-            transform: ['es6ify']
-        }))
-        .pipe($.uglify())
-        .pipe(gulp.dest('components/dist'));
+  gulp.src([
+    'src/x-gif.js',
+    'src/x-gif.angular.js',
+    'src/x-gif.raw.js'
+  ])
+    .pipe($.plumber())
+    .pipe($.browserify({
+      add: [ es6ify.runtime ],
+      transform: ['es6ify']
+    }))
+    .pipe($.uglify())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('html', function () {
-    gulp.src('components/mako-cube/src/mako-cube.html')
-        .pipe($.rename('mako-cube.local.html'))
-        .pipe(gulp.dest('components/dist'));
-});
+  gulp.src('src/x-gif.html')
+    .pipe($.rename('x-gif.local.html'))
+    .pipe(gulp.dest('dist'));
+})
 
 gulp.task('css', function () {
-    gulp.src('components/mako-cube/src/mako-cube.scss')
-        .pipe($.rubySass())
-        .pipe($.autoprefixer("last 2 versions", "> 1%"))
-        .pipe(gulp.dest('components/dist'));
-});
+  gulp.src('src/x-gif.scss')
+    .pipe($.rubySass())
+    .pipe($.autoprefixer("last 2 versions", "> 1%"))
+    .pipe(gulp.dest('dist'));
+})
 
 gulp.task('vulcanize', function () {
-    gulp.src('components/dist/mako-cube.local.html')
-        .pipe($.vulcanize({dest: 'components/dist', inline: true}))
-        .pipe($.rename('mako-cube.html'))
-        .pipe(gulp.dest('components/dist'));
-});
+  gulp.src('dist/x-gif.local.html')
+    .pipe($.vulcanize({dest: 'dist', inline: true}))
+    .pipe($.rename('x-gif.html'))
+    .pipe(gulp.dest('dist'));
+})
 
 gulp.task('copy', function () {
-    gulp.src([
-        'bower_components/platform/platform.js',
-        'bower_components/polymer/polymer*',
-        'bower_components/polymer/layout*'
-    ])
-        .pipe(gulp.dest('components/dist'));
+  gulp.src([
+    'bower_components/platform/platform.js',
+    'bower_components/polymer/polymer*',
+    'bower_components/polymer/layout*',
+  ])
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['js', 'html', 'css', "copy", 'vulcanize']);
+gulp.task('build', ['js', 'html', 'css', 'copy', 'vulcanize']);
 
 gulp.task('default', ['build', 'connect'], function () {
-    gulp.watch(['components/mako-cube/src/*.*js'], ['js']);
-    gulp.watch(['components/mako-cube/src/*.html'], ['html']);
-    gulp.watch(['components/mako-cube/src/*.scss'], ['css']);
-    gulp.watch(['bower_components'], ['copy']);
-    gulp.watch(['components/dist/mako-cube.local.html', 'components/dist/mako-cube.js', 'components/dist/mako-cube.css'], ['vulcanize']);
+  gulp.watch(['src/*.*js'], ['js']);
+  gulp.watch(['src/*.html'], ['html']);
+  gulp.watch(['src/*.scss'], ['css']);
+  gulp.watch(['bower_components'], ['copy']);
+  gulp.watch(['dist/x-gif.local.html', 'dist/x-gif.js', 'dist/x-gif.css'], ['vulcanize']);
 
-    gulp.watch(['index.html', 'components/dist/**.*', 'demos/**.*'], function (event) {
-        return gulp.src(event.path)
-            .pipe($.connect.reload());
-    });
+  gulp.watch(['index.html', 'dist/**.*', 'demos/**.*'], function (event) {
+    return gulp.src(event.path)
+      .pipe($.connect.reload());
+  });
 });
 
 gulp.task('connect', function () {
-    $.connect.server({
-        root: [__dirname],
-        port: 8000,
-        livereload: {port: 8001}
-    });
+  $.connect.server({
+    root: [__dirname],
+    port: 8000,
+    livereload: {port: 8001}
+  })
 });
