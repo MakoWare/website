7291179538c8d214c3b9d62ec2d0a1aec19ef74f
yintothayang <yintothayang@gmail.com>
2014-12-06 21:56:18 -0800

    fuck photoswipe, shit

--- site/public/components/pages/juke-page/juke-page.js
+++ site/public/components/pages/juke-page/juke-page.js
@@ -4,14 +4,13 @@ Polymer('juke-page', {
 
         this.changeBackground();
         this.fadeIn();
-        console.log("juke page ready()");
 
+        console.log("juke page ready()");
     },
 
     changeBackground: function(){
         $('body' ).css('background-image', " -webkit-linear-gradient(-45deg, #0674ef, #00d3f4)");
-
-    },
+    }
 
 
 });
--- site/public/oldComponents/three-cube/animations/cubeAnimation1.js
+++
@@ -1,4 +0,0 @@
-var cubeAnimation1 = function(){
-    this.rotation.y += 0.01;
-    //this.rotation.x += 0.05;
-};
