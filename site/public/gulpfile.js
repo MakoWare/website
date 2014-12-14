a3d49ac683bbcb2b7b0978bae6227c9bdae2246c
yintothayang <yintothayang@gmail.com>
2014-12-14 01:01:57 -0500

    using gulp and npm now, because if you don't everyone thinks you're weird

---
+++ site/public/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')();
+
+gulp.task('default', ['connect'], function () {
+
+    gulp.watch(['index.html', 'components/**/**.*', 'css/**.*', 'images/**.*'], function (event) {
+        return gulp.src(event.path)
+            .pipe($.connect.reload());
+    });
+});
+
+gulp.task('connect', function () {
+    $.connect.server({
+        root: [__dirname],
+        port: 8002,
+        livereload: {port: 2983}
+    });
+});
