4fd779262ea01730a816c07d7c35d6f59824a60d
yintothayang <yintothayang@gmail.com>
2015-02-09 17:26:50 -0800

    added mobile navigation!

--- site/public/components/mako-nav/dist/mako-nav.js
+++ site/public/components/mako-nav/dist/mako-nav.js
@@ -10,11 +10,35 @@ var makoNavController = function makoNavController(makoNav) {
     this.shadow = this.makoNav.createShadowRoot();
     var template = owner.querySelector("#template").content.cloneNode(true);
     this.shadow.appendChild(template);
+    $(this.makoNav.shadowRoot.querySelector("#burger")).on("click", this.onBurgerClicked.bind(this));
+    $(this.makoNav.shadowRoot.querySelector("#d1")).click(this.onLinkClicked.bind(this));
+    $(this.makoNav.shadowRoot.querySelector("#d2")).click(this.onLinkClicked.bind(this));
+    $(this.makoNav.shadowRoot.querySelector("#d3")).click(this.onLinkClicked.bind(this));
+    this.mobileNav = false;
   },
   srcChanged: function(src) {
     if (!src)
       return;
     console.log("Loading " + src);
+  },
+  onBurgerClicked: function() {
+    var mobileNav = this.makoNav.shadowRoot.querySelector("#mobileNav");
+    if (this.mobileNav) {
+      $(mobileNav).hide();
+      this.mobileNav = false;
+    } else {
+      var windowHeight = $(window).height();
+      var windowWidth = $(window).width();
+      $(mobileNav).height(windowHeight);
+      $(mobileNav).width(windowWidth);
+      $(mobileNav).slideToggle("slow");
+      this.mobileNav = true;
+    }
+  },
+  onLinkClicked: function() {
+    var mobileNav = this.makoNav.shadowRoot.querySelector("#mobileNav");
+    $(mobileNav).hide();
+    this.mobileNav = false;
   }
 }, {});
 var makoNav = function makoNav() {
--- site/public/components/mako-nav/src/mako-nav.js
+++ site/public/components/mako-nav/src/mako-nav.js
@@ -6,6 +6,7 @@ class makoNavController {
         this.makoNav = makoNav;
         this.setupComponent();
         console.log("makoNav");
+
     }
 
     setupComponent() {
@@ -15,12 +16,43 @@ class makoNavController {
         // stamp out our template in the shadow dom
         var template = owner.querySelector("#template").content.cloneNode(true);
         this.shadow.appendChild(template);
+        $(this.makoNav.shadowRoot.querySelector("#burger")).on("click", this.onBurgerClicked.bind(this));
+        $(this.makoNav.shadowRoot.querySelector("#d1")).click(this.onLinkClicked.bind(this));
+        $(this.makoNav.shadowRoot.querySelector("#d2")).click(this.onLinkClicked.bind(this));
+        $(this.makoNav.shadowRoot.querySelector("#d3")).click(this.onLinkClicked.bind(this));
+
+        this.mobileNav = false;
     }
 
     srcChanged(src) {
         if (!src) return;
         console.log("Loading " + src);
     }
+
+    onBurgerClicked() {
+        var mobileNav = this.makoNav.shadowRoot.querySelector("#mobileNav");
+
+        if(this.mobileNav){
+            $(mobileNav).hide();
+            this.mobileNav = false;
+        } else {
+
+            var windowHeight = $(window).height();
+            var windowWidth = $(window).width();
+            $(mobileNav).height(windowHeight);
+            $(mobileNav).width(windowWidth);
+            $(mobileNav).slideToggle("slow");
+            this.mobileNav = true;
+        }
+    }
+
+    onLinkClicked(){
+        var mobileNav = this.makoNav.shadowRoot.querySelector("#mobileNav");
+        $(mobileNav).hide();
+
+        this.mobileNav = false;
+    }
+
 }
 
 class makoNav extends HTMLElement {
