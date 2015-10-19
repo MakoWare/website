cbd359e0ced51e231a8d8be0b1fcf7ec3a3f45c2
yintothayang <yintothayang@gmail.com>
2015-10-18 21:22:13 -0700

    logging

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash, make_response, send_file, send_from_directory
-
+import logging
 
 app = Flask(__name__)
 
